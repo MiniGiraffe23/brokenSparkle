2e996fad87223584288c6a979c7fec2fc258e233
Brittany Baranowski <brittanybaranowski@gmail.com>
2017-05-26 11:24:50 -0500

    Update gem file, add seeds to seed file

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+10.times do
+  User.create!(email: Faker::Internet.safe_email, password: "password", username: Faker::HarryPotter.character)
+end
+users = User.all
+bool = [true, false]
+
+10.times do
+  Game.create!(title: Faker::Book.title, genre: Faker::Book.genre, couch_coop: bool.sample, multiplayer: bool.sample)
+end
+
+games = Item.all
+
+10.times do
+  Favorite.create!(user: users.sample, game: games.sample)
+end
